5c7bc7fadda5bcd21dac3138b77a3b4152ea2746
Stephen <ankustephen99@gmail.com>
2025-09-24 15:31:36 -0400

    Questions 4,5,6 solutions added

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -17,11 +17,25 @@ public class MathApp {
         System.out.println("The lowest price is: " + lowestPrice);
 
         //Question 3
-        float radiusCircle = 7.25;
+        float radiusCircle = (float) 7.25;
         float circleArea = (float) (Math.pow(radiusCircle, 2) * Math.PI);
         System.out.println("The circle area is: " + circleArea);
 
         //Question 4
+        float variableOne = (float) 5.0;
+        float squareRootVariable = (float) Math.sqrt(variableOne);
+        System.out.println("The square root of " + variableOne + " is: " + squareRootVariable);
+
+        //Question 5
+        int distanceXCoordinates = 5 - 85;
+        int distanceYCoordinates = 10 - 50;
+        float distanceBetweenPoints = (float) Math.hypot(distanceXCoordinates, distanceYCoordinates);
+        System.out.println("The distance between the points at (5,10) and (85,50) is: " + distanceBetweenPoints);
+
+        //Question 6
+        float variableTwo = (float) -3.8;
+        float absValueVariableTwo = (float) Math.abs(variableTwo);
+        System.out.println("The absolute value of " + variableTwo + " is: " + absValueVariableTwo);
 
     }
 }
